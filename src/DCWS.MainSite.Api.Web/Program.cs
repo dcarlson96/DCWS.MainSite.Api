07cb7f09c184b39169e812d5232072c8bb9f58a2
Dylan Carlson <dylan.dylancarlson@gmail.com>
2026-08-12 18:30:55 -0600

    Updating Cors to allow main site to hit api

--- src/DCWS.MainSite.Api.Web/Program.cs
+++ src/DCWS.MainSite.Api.Web/Program.cs
@@ -6,6 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MainSite", policy =>
+    {
+        policy
+            .WithOrigins(
+                "https://dcwebsystems.com",
+                "https://www.dcwebsystems.com")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddSingleton<IStatusService, StatusService>();
 
 var app = builder.Build();
@@ -17,6 +31,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("MainSite");
+
 app.MapControllers();
 
 app.Run();
